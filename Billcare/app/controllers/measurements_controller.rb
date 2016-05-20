a9dbd7b62d14583454dbc58e467252b8b5d79c6b
orrios <orrios@uc.cl>
2016-05-19 23:08:29 -0400

    correcta indexacion de mediciones

--- Billcare/app/controllers/measurements_controller.rb
+++ Billcare/app/controllers/measurements_controller.rb
@@ -19,7 +19,6 @@ class MeasurementsController < ApplicationController
     @data1 = Measurement.all.sort_by{|measure| measure.time}
     @data1 = @data1.map {|measure| {x: measure.time*1000, y:measure.value}}
     @data1 = @data1
-    winter_limit = @data1.last[:y]
     @min_time = @data1.first[:x]
     @max_time = @min_time+(1000*60*5) #los 1000 es un segundo. se quieren ver 5 minutos
     @data2 = [[@min_time,0.05],[@max_time, 0.05]]
@@ -45,9 +44,13 @@ class MeasurementsController < ApplicationController
 
   def addition
     if Measurement.first
-      Measurement.create :value => params[:value].to_f*4*220/(1000*3600)+Measurement.last.value.to_f, :time => params[:time].to_i # *4*220/(1000*3600) es para transformar a Kw/h
+      ordered = Measurement.all.sort_by do |item|
+        item.time
+      end
+      Measurement.create :value => params[:value].to_f*4*220/(1000*3600)+ordered.last.value.to_f, :time => params[:time].to_i # *4*220/(1000*3600) es para transformar a Kw/h
       if Measurement.last.time - Measurement.first.time > 300
         Measurement.delete_all
+        Measurement.create :value => params[:value].to_f*4*220/(1000*3600), :time => params[:time].to_i
       end
     else
       Measurement.create :value => params[:value].to_f*4*220/(1000*3600), :time => params[:time].to_i
